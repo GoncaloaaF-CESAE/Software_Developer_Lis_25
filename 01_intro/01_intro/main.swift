9fab0347fc7573dbf060ff5d1a4d1dc38d6c530a
GoncaloaaF <goncaloaaf@gmail.com>
2025-09-02 11:03:04 +0100

    introdução v1 - var e tipos de dados

---
+++ 01_intro/01_intro/main.swift
@@ -0,0 +1,141 @@
+//
+//  main.swift
+//  01_intro
+//
+//  Created by Gonçalo Feliciano on 02/09/2025.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("Hello, World2!")
+
+
+
+// cmt 1 linha
+
+/*
+ 
+ cmt
+ 
+ multi
+ 
+ linha
+ 
+ */
+
+
+/*
+ 
+ cmt - Done
+ 
+ var
+ const
+ nome var
+ 
+ op var
+ 
+ condições - if / switch
+
+ loops - for,  while, "do-While"
+ 
+ 
+ */
+
+
+/*
+ var
+ tipos de var
+ const
+ nome var
+ 
+
+ */
+
+/*
+ 
+ $nome = "Gonçalo"
+ $nome = 2000
+ 
+ */
+
+var nome = "Gonçalo" // inferencia de tipos // String nome = "Gonçalo";
+print(nome)
+
+
+var idade:Int // tipos explicito // int idade;
+
+
+nome = "João"
+print(nome)
+
+
+print(type(of: nome))
+print(type(of: idade))
+
+
+print("-----------")
+var num1 = 10
+print(type(of: num1))
+
+var num2:Int = 10
+print(type(of: num2))
+
+
+var num3 = 10.4
+print(type(of: num3))
+
+var num4:Double = 10.4
+print(type(of: num4))
+
+
+var num5:Float = 10.4
+print(type(of: num5))
+
+
+var foo = true
+print(type(of: foo))
+
+var foo2:Bool
+
+
+print("-----------")
+
+
+/*
+ 
+ int
+ 
+ int8
+ int16
+ int32 <- java
+ int64
+ int128
+ 
+ */
+
+
+print(Int8.min)
+print(Int8.max)
+
+print("-----------")
+print(Int.min)
+print(Int64.min)
+print("--")
+print(Int.max)
+print(Int64.max)
+print("-----------")
+
+print(Int128.min)
+print(Int128.max)
+
+
+print("-----------")
+
+print(UInt8.min)
+print(UInt8.max)
+
+var foo4:UInt8
+
+foo4 = 12
+
